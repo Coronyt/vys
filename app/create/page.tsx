1254effacb39b9ca71a6dfa8a623eb08c415f4a1
Coronyt <coronyt.apoelius@gmail.com>
2025-04-26 18:56:24 -0500

    Created pages for the order table and new order form

---
+++ app/create/page.tsx
@@ -0,0 +1,8 @@
+export default function Create() {
+    return (
+      <div className="page">
+        <h2>Create new order</h2>
+      </div>
+    );
+  }
+  
---
+++ app/inspect/page.tsx
@@ -0,0 +1,8 @@
+export default function Inspect() {
+    return (
+      <div className="page">
+        <h2>Inspect all orders</h2>
+      </div>
+    );
+  }
+  
