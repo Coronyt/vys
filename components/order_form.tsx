f993f27cefd150921a0418fdf259abbd8fb25ee5
Coronyt <coronyt.apoelius@gmail.com>
2025-04-27 20:06:53 -0500

    OrderForm refactored to client component utilizing React states

--- components/order_form.tsx
+++ components/order_form.tsx
@@ -1,13 +1,43 @@
+'use client';
+
+import { Order } from "@/interfaces/Order";
+import { Resource } from "@/interfaces/Resource";
+import { useState } from "react";
+
 export default function OrderForm() {
+
+  const [name, setName] = useState("New order");
+  const [desc, setDesc] = useState("New order description");
+
+  const date = new Date();
+
+  const submit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <form className="order_form">
+    <form className="order_form" onSubmit={(e) => submit(e)}>
       <div className="form_segment">
         <label htmlFor="name_input">Order name:</label>
-        <input type="text" name="name_input" id="name_input" className="name_input"/>
+        <input
+          type="text"
+          name="name_input"
+          id="name_input"
+          className="name_input"
+          value={name}
+          onChange={(e) => {setName(e.target.value)}}
+        />
       </div>
       <div className="form_segment">
         <label htmlFor="desc_input">Description:</label>
-        <textarea name="desc_input" id="desc_input" className="desc_input"/>
+        <textarea
+          name="desc_input"
+          id="desc_input"
+          className="desc_input"
+          value={desc}
+          onChange={(e) => {setDesc(e.target.value)}}
+        />
       </div>
       <div className="form_segment">
         <label htmlFor="res_input">Assign resource:</label>
@@ -23,11 +53,23 @@ export default function OrderForm() {
       </div>
       <div className="form_segment">
         <label htmlFor="start_input">Start date:</label>
-        <input type="date" name="start_input" id="start_input"/>
+        <input
+          type="date"
+          name="start_input"
+          id="start_input"
+          // value={start}
+          // onChange={(e) =>{setStart(e.target.value)}}
+        />
       </div>
       <div className="form_segment">
         <label htmlFor="end_input">End date:</label>
-        <input type="date" name="end_input" id="end_input"/>
+        <input
+          type="date"
+          name="end_input"
+          id="end_input"
+          // value={end}
+          // onChange={(e) =>{setEnd(e.target.value)}}
+        />
       </div>
       <div className="form_submit">
         <input type="submit" value="Create order"/>
--- interfaces/Order.ts
+++ interfaces/Order.ts
@@ -4,6 +4,6 @@ export interface Order {
     name: string,
     desc: string,
     res: Resource,
-    start: Date,
-    end: Date
+    start: number,
+    end: number
 }
