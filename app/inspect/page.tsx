d84ece532beaa36e00ef8d773fa55e7d195d64c4
Coronyt <coronyt.apoelius@gmail.com>
2025-04-29 13:55:29 -0500

    Installed TanStack Table and created Table component

--- app/inspect/page.tsx
+++ app/inspect/page.tsx
@@ -1,43 +1,11 @@
 'use client'; // FOR TESTING PURPOSES ONLY
 
-import { useEffect } from "react";
+import Table from "@/components/order_table";
 import { useOrderContext } from "@/context/order_context";
 
 export default function Inspect() {
 
   const { orders, setOrders } = useOrderContext();
-
-  useEffect(() => {
-    const test_date = {
-      month: 11,
-      day: 11,
-      year: 1918,
-      hour: 11,
-      minute: 0
-    }
-    const order1 = {
-      name: "Test order 1",
-      desc: "Test description",
-      res: {name: "res1", id: "2814", in_use: false},
-      start: test_date,
-      end: test_date
-    }
-    const order2 = {
-      name: "Test order 2",
-      desc: "Test description",
-      res: {name: "res2", id: "2814", in_use: false},
-      start: test_date,
-      end: test_date
-    }
-    const order3 = {
-      name: "Test order 3",
-      desc: "Test description",
-      res: {name: "res3", id: "2814", in_use: false},
-      start: test_date,
-      end: test_date
-    }
-    // setOrders([order1, order2, order3]);
-  }, []);
 
   return (
     <div className="page">
@@ -53,6 +21,7 @@ export default function Inspect() {
           {element.end.month + "-" + element.end.day + "-" + element.end.year} <br /> <br />
         </p>
       )}
+      <Table></Table>
     </div>
   );
 }
---
+++ components/order_table.tsx
@@ -0,0 +1,7 @@
+export default function Table() {
+    return (
+      <div>
+        <h2>order_table</h2>
+      </div>
+    );
+  }
