11ff1617b3b5c8eff6bf4ab608ab7150c4cbb87c
Coronyt <coronyt.apoelius@gmail.com>
2025-04-29 15:49:31 -0500

    Began TanStack Table implementation

--- components/order_table.tsx
+++ components/order_table.tsx
@@ -1,7 +1,37 @@
+'use client';
+
+import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+
+import { useOrderContext } from "@/context/order_context";
+
 export default function Table() {
+
+    const columns = [
+        {
+            accessorKey: 'name',
+            header: 'name_header',
+            cell: (props: any) => {
+                <p>{props.getValue}</p>
+            }
+        }
+    ]
+
+    const { orders, setOrders } = useOrderContext();
+
+    const table = useReactTable({
+      data: orders,
+      columns,
+      getCoreRowModel: getCoreRowModel()
+    });
+
     return (
       <div>
-        <h2>order_table</h2>
+        <h2>order_table</h2> <br /> <hr /> <br />
+        {table.getHeaderGroups().map((header_group) => {
+          return <div>{header_group.headers.map((header) => {
+            return <p>{`${header.column.columnDef.header}`}</p>
+          })}</div>
+        })}
       </div>
     );
   }
