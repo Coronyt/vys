b79784dd45289b14713d9d3838a48eb0cd0bc31c
Coronyt <coronyt.apoelius@gmail.com>
2025-05-03 18:46:51 -0500

    DateTimeCell will display a date string built from the Order's DateTime

--- components/cells/date_time.tsx
+++ components/cells/date_time.tsx
@@ -1,11 +1,32 @@
+import { DateTime, build_date, build_date_display } from "@/interfaces/DateTime";
+
 import { useState } from "react";
 
 export default function DateTimeCell(props: any) {
 
+    console.log(props.cell.getValue());
+    console.log(build_date(props.cell.getValue()));
+    // const [dateText, setDateText] = useState(props.cell.getValue());
+
     return (
         <div className="flex justify-center">
-            <input type="date" />
-            <input type="time" />
+            {/* <input type="date" /> */}
+            {/* <input type="time" /> */}
+            <input
+                type="text"
+                value={build_date_display(props.cell.getValue())}
+                // onChange={(e) => {setText(e.target.value)}}
+                // onBlur={blur}
+                className="w-24 text-center"
+            />
+            <div className="w-2" />
+            <input
+                type="text"
+                value={"12:00 PM"}
+                // onChange={(e) => {setText(e.target.value)}}
+                // onBlur={blur}
+                className="w-24 text-center"
+            />
         </div>
     );
   }
--- interfaces/DateTime.ts
+++ interfaces/DateTime.ts
@@ -1,11 +1,29 @@
-export const build_date = (dt: DateTime): String => {
-    // TODO - Accept a DateTime and return a valid Date string
-    return "";
+// Will return a date string formatted for Zod validation
+export const build_date = (dt: DateTime): string => {
+    let date_str = `${dt.year}`;
+    if (dt.month < 10) {date_str += "-0" + dt.month} else {date_str += "-" + dt.month}
+    if (dt.day < 10) {date_str += "-0" + dt.day} else {date_str += "-" + dt.day}
+    return date_str;
 }
 
-export const build_time = (dt: DateTime): String => {
-    // TODO - Accept a DateTime and return a valid Time string
-    return "";
+// Will return a date string formatted for user readability
+export const build_date_display = (dt: DateTime): string => {
+    let date_str = "";
+    if (dt.month < 10) {date_str += "0" + dt.month} else {date_str += dt.month}
+    if (dt.day < 10) {date_str += "-0" + dt.day} else {date_str += "-" + dt.day}
+    return date_str += "-" + dt.year;
+}
+
+// Will return a time string formatted for Zod validation
+export const build_time = (dt: DateTime): string => {
+    let time_str = "";
+    return time_str;
+}
+
+// Will return a time string formatted for user readability
+export const build_time_display = (dt: DateTime): string => {
+    let time_str = "";
+    return time_str;
 }
 
 export interface DateTime {
