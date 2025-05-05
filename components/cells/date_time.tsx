f103bf93dc4460593dae15b643991815431d30c6
Coronyt <coronyt.apoelius@gmail.com>
2025-05-05 18:18:28 -0500

    Edit Order start/end times directly from Table (w/ Zod validation)

--- components/cells/date_time.tsx
+++ components/cells/date_time.tsx
@@ -1,4 +1,4 @@
-import { build_date_display, display_to_formal, formal_to_datetime } from "@/interfaces/DateTime";
+import { build_date_display, build_time_display, display_to_formal_date, display_to_formal_time, formal_date_to_datetime, formal_time_to_datetime } from "@/interfaces/DateTime";
 
 import { useState } from "react";
 import { z } from "zod";
@@ -6,24 +6,46 @@ import { z } from "zod";
 export default function DateTimeCell(props: any) {
 
     const zdate = z.string().date();
+    const ztime = z.string().time();
 
     const original_date = build_date_display(props.cell.getValue());
     const [dateText, setDateText] = useState(build_date_display(props.cell.getValue()));
 
-    const blur = () => {
+    const original_time = build_time_display(props.cell.getValue());
+    const [timeText, setTimeText] = useState(build_time_display(props.cell.getValue()));
+
+    const blur_date = () => {
         // Validate input string w/ Zod after converting format
-        const formal = display_to_formal(dateText);
+        const formal = display_to_formal_date(dateText);
         try {
             zdate.parse(formal);
             // After successful validation, update the value in the table
             props.table.options.meta.update(
                 props.row.index,
                 props.cell.column.id,
-                formal_to_datetime(formal, props.cell.getValue())
+                formal_date_to_datetime(formal, props.cell.getValue())
             );
         } catch (error) {
             // Otherwise reset the cell display value and do not update the table
             setDateText(original_date);
+        }
+    }
+
+    const blur_time = () => {
+        // Validate input string w/ Zod after converting format
+        const formal = display_to_formal_time(timeText);
+        try {
+            ztime.parse(formal);
+            console.log(formal);
+            // After successful validation, update the value in the table
+            props.table.options.meta.update(
+                props.row.index,
+                props.cell.column.id,
+                formal_time_to_datetime(formal, props.cell.getValue())
+            );
+        } catch (error) {
+            // Otherwise reset the cell display value and do not update the table
+            setTimeText(original_time);
         }
     }
 
@@ -33,15 +55,15 @@ export default function DateTimeCell(props: any) {
                 type="text"
                 value={dateText}
                 onChange={(e) => {setDateText(e.target.value)}}
-                onBlur={blur}
+                onBlur={blur_date}
                 className="w-24 text-center"
             />
             <div className="w-2" />
             <input
                 type="text"
-                value={"12:00 PM"}
-                onChange={() => {}}
-                onBlur={() => {}}
+                value={timeText}
+                onChange={(e) => {setTimeText(e.target.value)}}
+                onBlur={blur_time}
                 className="w-24 text-center"
             />
         </div>
--- interfaces/DateTime.ts
+++ interfaces/DateTime.ts
@@ -14,28 +14,69 @@ export const build_date_display = (dt: DateTime): string => {
     return date_str += "-" + dt.year;
 }
 
-// Will return a time string formatted for Zod validation
-export const build_time = (dt: DateTime): string => {
-    // TODO
-    let time_str = "";
-    return time_str;
-}
+// TODO - Will return a time string formatted for Zod validation
+// export const build_time = (dt: DateTime): string => {
+//     let time_str = "";
+//     return time_str;
+// }
 
 // Will return a time string formatted for user readability
 export const build_time_display = (dt: DateTime): string => {
-    // TODO
     let time_str = "";
+    if (dt.hour < 10) {
+        if (dt.hour < 1) { // Midnight (00:00 as 12:00 AM)
+            time_str += String(dt.hour + 12);
+        } else {
+            time_str += "0" + dt.hour;
+        }
+    } else {
+        if (dt.hour > 12) { // Afternoon (13:00 as 1:00 PM)
+            if (dt.hour == 22 || dt.hour == 23) {
+                // (10:00 PM - 11:00 PM)
+                time_str += String(dt.hour - 12);
+            } else { // (12:00 PM - 9:00 PM)
+                time_str += "0" + String(dt.hour - 12);
+            }
+        }
+        else {
+            time_str += dt.hour;
+        }
+    }
+    if (dt.minute < 10) {
+        time_str += ":0" + dt.minute;
+    } else {
+        time_str += ":" + dt.minute;
+    }
+    if (dt.hour >= 12) {
+        time_str += " PM"
+    } else {
+        time_str += " AM"
+    }
     return time_str;
 }
 
 // Will produce a valid Zod date string if the input is formatted MM-DD-YYYY
-export const display_to_formal = (dis: string): string => {
+export const display_to_formal_date = (dis: string): string => {
     const mdy = dis.split("-"); // [0] = month, [1] = day, [2] = year
     return mdy[2] + "-" + mdy[0] + "-" + mdy[1]; // Return as YYYY-MM-DD
 }
 
+// TODO
+// TODO - AM and PM designations should not be case-sensitive
+export const display_to_formal_time = (dis: string): string => {
+    let h: number = Number(dis.split(" ")[0].split(":")[0]);
+    let m: string = dis.split(" ")[0].split(":")[1];
+    if (dis.split(" ")[1] == "PM" && h !== 12) {h += 12} // Noon
+    if (dis.split(" ")[1] == "AM" && h == 12) {h -= 12} // Midnight
+    if (h < 10) {
+        return "0" + h + ":" + m;
+    } else {
+        return "" + h + ":" + m;
+    }
+}
+
 // Will accept a Zod date string and a DateTime and return a new DateTime with the new date
-export const formal_to_datetime = (date: string, datetime: DateTime): DateTime => {
+export const formal_date_to_datetime = (date: string, datetime: DateTime): DateTime => {
     const ymd = date.split("-"); // [0] = year, [1] = month, [2] = day
     return {
         month: Number(ymd[1]),
@@ -43,6 +84,17 @@ export const formal_to_datetime = (date: string, datetime: DateTime): DateTime =
         year: Number(ymd[0]),
         hour: datetime.hour,
         minute: datetime.minute
+    }
+}
+
+// TODO
+export const formal_time_to_datetime = (time: string, datetime: DateTime): DateTime => {
+    return {
+        month: datetime.month,
+        day: datetime.day,
+        year: datetime.year,
+        hour: Number(time.split(":")[0]),
+        minute: Number(time.split(":")[1])
     }
 }
 
