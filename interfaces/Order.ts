f5bdd3381563b6603212cc4257b39e57fe12d32d
Coronyt <coronyt.apoelius@gmail.com>
2025-05-05 22:56:29 -0500

    update_status() will detect and update Order status

--- interfaces/Order.ts
+++ interfaces/Order.ts
@@ -1,15 +1,34 @@
 import { DateTime, build_date, build_time } from "./DateTime"
 
+import { z } from "zod";
+
+const zdate = z.string().date();
+const ztime = z.string().time();
+
 export const update_status_all = (orders: Order[]) => {
     orders.forEach((order) => {update_status(order)});
 }
 
 export const update_status = (order: Order) => {
-    // console.log(new Date());
-    // console.log(new Date("2025-01-01 13:00"));
-    const curr_dt = new Date();
-    // console.log(build_date(order.start));
-    // console.log(build_time(order.start));
+    const curr_date = new Date();
+    try {
+        zdate.parse(build_date(order.start));
+        ztime.parse(build_time(order.start));
+        zdate.parse(build_date(order.end));
+        ztime.parse(build_time(order.end));
+        order.status = Status.SCHEDULED;
+        const start_date = new Date(build_date(order.start) + " " + build_time(order.start));
+        const end_date = new Date(build_date(order.end) + " " + build_time(order.end));
+        if (curr_date > start_date) {
+            if (curr_date < end_date) {
+                order.status = Status.ACTIVE;
+            } else {
+                order.status = Status.COMPLETED;
+            }
+        }
+    } catch (error) {
+        order.status = Status.PENDING;
+    }
 }
 
 export enum Status {
