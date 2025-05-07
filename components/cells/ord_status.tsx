df8268c9650eb80c7b2595b449fb4cef9d796d5c
Coronyt <coronyt.apoelius@gmail.com>
2025-05-07 17:15:33 -0500

    Table renders entries according to active filter

--- components/cells/ord_status.tsx
+++ components/cells/ord_status.tsx
@@ -2,8 +2,6 @@ import DeleteOrderCell from "./del_order";
 import { useState } from "react";
 
 export default function OrderStatusCell(props: any) {
-    
-    // console.log(props.cell.getValue());
 
     const [status, setStatus] = useState(props.cell.getValue());
 
@@ -11,15 +9,15 @@ export default function OrderStatusCell(props: any) {
         <div>
             {/* Status cell */}
             <div className={`flex justify-center rounded-sm
-                ${status == 0 ? "bg-rose-600/50" : ""}
-                ${status == 1 ? "bg-blue-600/50" : ""}
-                ${status == 2 ? "bg-lime-600/50" : ""}
-                ${status == 3 ? "bg-zinc-600/25" : ""}
+                ${status == "0" ? "bg-rose-600/50" : ""}
+                ${status == "1" ? "bg-blue-600/50" : ""}
+                ${status == "2" ? "bg-lime-600/50" : ""}
+                ${status == "3" ? "bg-zinc-600/25" : ""}
             `}>
-                {status === 0 && "Pending"}
-                {status === 1 && "Scheduled"}
-                {status === 2 && "Active"}
-                {status === 3 && "Completed"}
+                {status === "0" && "Pending"}
+                {status === "1" && "Scheduled"}
+                {status === "2" && "Active"}
+                {status === "3" && "Completed"}
             </div>
             {/* Delete order */}
             <div className="relative left-60 -top-7 w-10">
--- components/order_table.tsx
+++ components/order_table.tsx
@@ -12,89 +12,117 @@ import { useOrderContext } from "@/context/order_context";
 
 export default function Table() {
 
-  const [filters, setFilters] = useState([
-    // {
-    //   id: "status",
-    //   value: Status.PENDING
-    // }
+  const [columnFilters, setColumnFilters] = useState([
+    {
+      id: "status",
+      value: ""
+    }
   ]);
 
-    const columns = [
-      {
-          accessorKey: 'name',
-          header: 'Order'
-      },
-      // {
-      //   accessorKey: 'desc',
-      //   header: 'Description'
-      // },
-      {
-        accessorKey: 'res',
-        header: 'Resource'
-      },
-      {
-        accessorKey: 'start',
-        header: 'Starting'
-      },
-      {
-        accessorKey: 'end',
-        header: 'Ending'
-      },
-      {
-        accessorKey: 'status',
-        header: 'Status'
-      }
-    ]
-
-    const { orders, setOrders } = useOrderContext();
-    
-    useEffect(() => {
-      // update_status_all();
-      // console.log(orders);
-      let statuses: Status[] = [];
-      orders.forEach((order) => {
-        statuses.push(update_status(order));
-      });
-      let i: number = 0;
-      let updated: Order[] = Array.from(orders);
-      updated.forEach((u_order) => {
-        u_order.status = statuses[i];
-        i++;
-      });
-      // console.log(updated);
-      setOrders(updated);
-    }, []);
-
-    const table = useReactTable({
-      data: orders,
-      columns,
-      state: {
-        columnFilters: filters
-      },
-      getCoreRowModel: getCoreRowModel(),
-      getFilteredRowModel: getFilteredRowModel(),
-      meta: {
-        // TODO - Add comments to this code
-        update: (row_index: number, column_id: string, value: any) => {
-          setOrders(
-            prev => prev.map(
-              (row, index) =>
-                index === row_index ? {
-                  ...prev[row_index],
-                  [column_id]: value
-                }: row
-            )
-          );
-        }
-      }
-    });
-
-    return (
-      <div>
-        <h2 className="page_title">View all orders</h2>
-        <FilterSwitch></FilterSwitch>
-        <TableHeader table={table}></TableHeader>
-        <TableEntries table={table}></TableEntries>
-      </div>
-    );
+  const cycle_filter = (): string => {
+    if (columnFilters[0].value == "") {
+      setColumnFilters([{ id: "status", value: "0" }]);
+      return "Pending orders";
+    }
+    if (columnFilters[0].value == "0") {
+      setColumnFilters([{ id: "status", value: "1" }]);
+      return "Scheduled orders";
+    }
+    if (columnFilters[0].value == "1") {
+      setColumnFilters([{ id: "status", value: "2" }]);
+      return "Active orders";
+    }
+    if (columnFilters[0].value == "2") {
+      setColumnFilters([{ id: "status", value: "3" }]);
+      return "Completed orders";
+    }
+    if (columnFilters[0].value == "3") {
+      setColumnFilters([{ id: "status", value: "" }]);
+      return "No filter applied";
+    }
+    return "";
   }
+
+  const columns = [
+    {
+        accessorKey: 'name',
+        header: 'Order'
+    },
+    // {
+    //   accessorKey: 'desc',
+    //   header: 'Description'
+    // },
+    {
+      accessorKey: 'res',
+      header: 'Resource'
+    },
+    {
+      accessorKey: 'start',
+      header: 'Starting'
+    },
+    {
+      accessorKey: 'end',
+      header: 'Ending'
+    },
+    {
+      accessorKey: 'status',
+      header: 'Status',
+      accessorFn: (row: any) => {
+        console.log(`${row.status}`);
+        return `${row.status}`;
+      }
+    }
+  ]
+
+  const { orders, setOrders } = useOrderContext();
+  
+  useEffect(() => {
+    // update_status_all();
+    // console.log(orders);
+    let statuses: Status[] = [];
+    orders.forEach((order) => {
+      statuses.push(update_status(order));
+    });
+    let i: number = 0;
+    let updated: Order[] = Array.from(orders);
+    updated.forEach((u_order) => {
+      u_order.status = statuses[i];
+      i++;
+    });
+    // console.log(updated);
+    setOrders(updated);
+  }, []);
+
+  const table = useReactTable({
+    data: orders,
+    columns,
+    state: {
+      columnFilters
+    },
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    meta: {
+      // TODO - Add comments to this code
+      update: (row_index: number, column_id: string, value: any) => {
+        setOrders(
+          prev => prev.map(
+            (row, index) =>
+              index === row_index ? {
+                ...prev[row_index],
+                [column_id]: value
+              }: row
+          )
+        );
+      }
+    }
+  });
+
+  return (
+    <div>
+      <h2 className="page_title">View all orders</h2>
+      <FilterSwitch cycle={cycle_filter} />
+      <TableHeader table={table}></TableHeader>
+      <TableEntries table={table}></TableEntries>
+    </div>
+  );
+}
