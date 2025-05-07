55a181e53fb23009594bb6f252e298a835412049
Coronyt <coronyt.apoelius@gmail.com>
2025-05-06 19:03:50 -0500

    OrderStatusCell will change background color depending on status

--- components/cells/ord_status.tsx
+++ components/cells/ord_status.tsx
@@ -10,11 +10,16 @@ export default function OrderStatusCell(props: any) {
     return (
         <div>
             {/* Status cell */}
-            <div className="flex justify-center bg-amber-800 rounded-sm">
-                {props.cell.getValue() === 0 && "Pending"}
-                {props.cell.getValue() === 1 && "Scheduled"}
-                {props.cell.getValue() === 2 && "Active"}
-                {props.cell.getValue() === 3 && "Completed"}
+            <div className={`flex justify-center rounded-sm
+                ${status == 0 ? "bg-rose-600/50" : ""}
+                ${status == 1 ? "bg-blue-600/50" : ""}
+                ${status == 2 ? "bg-lime-600/50" : ""}
+                ${status == 3 ? "bg-zinc-600/25" : ""}
+            `}>
+                {status === 0 && "Pending"}
+                {status === 1 && "Scheduled"}
+                {status === 2 && "Active"}
+                {status === 3 && "Completed"}
             </div>
             {/* Delete order */}
             <div className="relative left-60 -top-7 w-10">
