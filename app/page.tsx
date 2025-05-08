0ec3998fe23e3f3e19d8f69c0a412ecfcef66189
Coronyt <coronyt.apoelius@gmail.com>
2025-05-07 21:38:20 -0500

    Implemented OrderOverview pie chart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import OrderOverview from "@/components/charts/order_overview";
+
 export default function Dash() {
   return (
     <div className="page">
       <h2 className="page_title">
         Dashboard
       </h2>
+      <OrderOverview />
     </div>
   );
 }
---
+++ components/charts/order_overview.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+import { useEffect, useState } from "react";
+
+import { useOrderContext } from "@/context/order_context";
+
+export default function OrderOverview() {
+
+    interface ChartData {
+        name: string,
+        value: number
+    }
+
+    let data: ChartData[] = [
+      {
+        name: "Pending",
+        value: 0
+      },
+      {
+        name: "Scheduled",
+        value: 0
+      },
+      {
+        name: "Active",
+        value: 0
+      }
+    ]
+
+    const colors = [
+      "#730424",
+      "#0e2d7d",
+      "#2c5404"
+    ]
+
+    const [chartData, setChartData] = useState(data);
+    const { orders, setOrders } = useOrderContext();
+
+    useEffect(() => {
+      orders.forEach((order) => {
+        if (order.status == 0) {data[0].value++}
+        if (order.status == 1) {data[1].value++}
+        if (order.status == 2) {data[2].value++}
+      });
+      setChartData(data);
+    }, []);
+
+    return (
+      <div>
+        <ResponsiveContainer width={730} height={250}>
+          <PieChart width={730} height={250}>
+            <Tooltip />
+            <Legend formatter={(value, entry, index) => (<span className="text-white">{value}</span>)} />
+            <Pie data={chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100}>
+              {chartData.map((entry, index) => (<Cell key={`cell-${index}`} fill={colors[index]} />))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  }
